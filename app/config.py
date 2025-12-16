49c9f47245bff01c95007c68f8342ecfad34bae1
Artem Arkhipov <amarkhipov@kpl.ru>
2025-12-16 16:31:34 +0300

    step 7: change to sync driver db

--- app/config.py
+++ app/config.py
@@ -7,5 +7,8 @@ load_dotenv()
 BASE_URL = os.getenv("BASE_URL", "http://localhost:8000")
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql+asyncpg://user:password@localhost:5432/urlshortener?sslmode=disable",
+    "postgresql+psycopg2://user:password@localhost:5432/urlshortener?sslmode=disable",
 )
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg://")
