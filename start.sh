4831dd1e4efe5f624918a97800f37e490d670ffb
Artem Arkhipov <amarkhipov@kpl.ru>
2025-12-16 16:35:53 +0300

    step 7: change host

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ PORT=${PORT:-80}
 
 echo "Starting backend on port 8080..."
 # Запускаем backend в фоне на порту 8080
-uv run uvicorn app.main:app --host 127.0.0.1 --port 8080 &
+uv run uvicorn app.main:app --host 0.0.0.0 --port 8080 &
 
 echo "Starting nginx on port $PORT..."
 
